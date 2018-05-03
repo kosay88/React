145a8a39753f4c38a27dbdba54c351e3eec1fadd
kosay88 <kosay.faraj.88@gmail.com>
2018-05-03 19:33:56 +0200

    starting with php

---
+++ PHP/help.php
@@ -0,0 +1,101 @@
+<?php
+// /* your code goes here */
+//
+//   echo "hello turing bootcampers";
+//   print "Happy coding";
+//   echo "Wow, I can print stuff";
+//
+//   echo "hello turing bootcampers <br>";
+//   echo "Wow, I can print stuff";
+//
+//   //This is a comment
+// //And another one
+//
+// /* we can add comments
+// over multiple lines
+// without needing to add
+// those annoying bars
+// everywhere */
+//
+// $name = "Alan";
+// echo 'My name is $name Turing.'; //My name is $name Turing.
+//
+// $num = 10;
+// $sum = $num + 10;
+// echo $sum //20
+//
+// $a = 5;
+// $b = 2.5;
+// #sum = $a + $b; //7.5
+//
+// $student = true;
+// $other = false;
+//
+// $clients = [];
+// var_dump($clients);
+//
+// array_push($clients, "Maksut");
+// var_dump($clients);
+//
+// echo $clients[0]; //Maksut
+
+// $grade_average = ["Maksut" => "8.2", "Andy" => "9.7", "Teodor" => "5.6");
+//
+// $grade_average["Maksut"] = "8.2";
+// $grade_average["Andy"] = "9.7";
+// $grade_average["Teodor"] = "5.6";
+
+// echo $grade_average['Teodor'] //5.6
+
+// $school = "turing bootcamp";
+// if($school = "RSM"){
+//   echo "I am an RSM student";
+// } else {
+//   echo "I am part of Turing Society";
+// }
+
+// Switch statement - a better way to check the truthfulness of a larger number of cases, you can imagine it as a bunch of if statements put together.
+// $name = "Alan";
+// $roles = ['guest','user','admin'];
+// $role = 0; //Key of the array $roles
+// switch($name){
+//   case 'Maksut':
+//     $role = 1;
+//   break;
+//   case 'Tomas':
+//     $role = 1;
+//   break;
+//   case 'Alan':
+//     $role =2;
+//   break;
+// }
+// echo "Welcome, $name! Your current account status is $roles[$role]";
+
+// for ($i = 0; $i <= 3; $i++)
+// {
+//   echo "Alan Turing ";
+// }
+
+// foreach($students as $key => $student){
+// // code to execute
+// }
+// $y = ["uno" => "one", "duos" => "two"];
+// foreach($y as $key => $value)
+// {
+//   echo $key . " - " . $value . "<br>";
+// }
+
+// $num = 5;
+// while($num < 20)
+// {
+//   echo $num;
+//   $num++;
+// }
+// $num = 5;
+// do
+// {
+//   echo $num;
+//   $num++;
+// } while($num < 20);
+
+?>
